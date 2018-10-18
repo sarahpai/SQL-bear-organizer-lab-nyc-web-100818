6e7bc96e3b76700ffd117b44935b596dacc5ed84
github username <github email address>
2018-10-18 11:51:06 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, color) VALUES ('Mr. Chocolate', 'Brown');
+INSERT INTO bears (name, color) VALUES ('Rowdy', 'Black');
+INSERT INTO bears (name, color) VALUES ('Tabitha', 'Grey');
+INSERT INTO bears (name, color) VALUES ('Sergeant Brown', 'Brown');
+INSERT INTO bears (name, color) VALUES ('Melissa', 'Pink');
+INSERT INTO bears (name, color) VALUES ('Grinch', 'Green');
+INSERT INTO bears (name, color) VALUES ('Wendy', 'White');
+INSERT INTO bears (name, color) VALUES (NULL, NULL);
+-- INSERT INTO fans (name, artist_id) VALUES ('Sarah', 169);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,61 @@
+#       SELECT columns
+#       FROM tables_name
+#       WHERE artists.Name = "Black Sabbath"
+# SELECT [names of columns we are going to select] FROM [table we are selecting from];
+# SELECT * FROM [table name] WHERE [column name] = [some value];
 def selects_all_female_bears_return_name_and_age
-  "Write your SQL query here"
+  'SELECT bears.name, bears.age FROM bears WHERE gender = "F"'
 end
 
+# ORDER BY column1, column2, ... ASC|DESC
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "Write your SQL query here"
+  'SELECT bears.name FROM bears ORDER BY name'
 end
 
+# SELECT column1, column2, ...
+# FROM table_name
+# WHERE condition1 AND condition2 AND condition3
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "Write your SQL query here"
+  'SELECT bears.name, bears.age
+  FROM bears WHERE alive = 1
+  ORDER BY age'
 end
 
+# SELECT MAX(column_name)
+# FROM table_name
+# WHERE condition;
+# oder by age limit by 1
 def selects_oldest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  'SELECT bears.name, bears.age FROM bears ORDER BY age DESC LIMIT 1'
 end
+# SELECT article FROM table1 ORDER BY publish_date LIMIT 20
+
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  'SELECT min(bears.name),(bears.age) FROM bears ORDER BY age ASC LIMIT 1'
 end
 
+# SELECT COUNT(CustomerID), Country
+# FROM Customers
+# GROUP BY Country
+# ORDER BY COUNT(CustomerID) DESC;
+# select all by bears.color, group by bears color, order by the color count DESC, limit by 1
 def selects_most_prominent_color_and_returns_with_count
-  "Write your SQL query here"
+  'SELECT bears.color, COUNT(bears.color)
+  FROM bears
+  GROUP BY bears.color
+  ORDER BY COUNT(bears.color) DESC LIMIT 1'
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  'SELECT COUNT(bears.temperament)
+  FROM bears
+  WHERE bears.temperament = "goofy"'
 end
 
 def selects_bear_that_killed_Tim
-  "Write your SQL query here"
+  'SELECT *
+  FROM bears
+  WHERE bears.name IS NULL
+  '
 end
